6f3941cd56f4cbd04b7e939290ad2fc2376b62ca
Oskar Borek <oborek@pega-sus.de>
2018-02-05 21:20:01 +0100

    ready

--- csv_script.php
+++ csv_script.php
@@ -7,17 +7,24 @@
  */
 
 
-$src_dir = "src/";
 $output_dir = "output/";
 $i=0;
 $filelist=array();
 $content="";
 // Change this variable...
-
+$spalte = 1;
+  
 //$col_name="Speed";
-$col_name=$argv[1];
 
-// echo $argv[1];
+if( $argv[1] == "noheads" ){
+  $col_name="file_without_heads";
+  $src_dir = "src_noheads/";
+  
+}else{
+  $col_name=$argv[1];
+  $src_dir = "src/";
+}
+
 
 //$output_file = $output_dir.'output_test.txt';
 $output_file = $output_dir.'output_average_'.$col_name.'.csv';
@@ -32,7 +39,7 @@ if (is_dir($src_dir)){
     if ($dh = opendir($src_dir)){
       while (($file = readdir($dh)) !== false){
         #echo "filename:" . $file . "\n";
-        if ( $file != "." && $file != ".."){
+        if ( $file != "." && $file != ".." && $file != ".DS_Store"){
           $filelist[$i] = $file; 
           $i++;
         }
@@ -43,7 +50,6 @@ if (is_dir($src_dir)){
     }
   }
 
-// var_dump($filelist);  
 
 // Loop: Run threw file list
 
@@ -52,75 +58,44 @@ if (is_dir($src_dir)){
     while($i < count($filelist))
     {
       $j = 0; $sum=0; $content="";
-      $current_csv = csv_to_array("src/".$filelist[$i]);
-      
-      $content .= "File: ;".$filelist[$i].";";
 
-      # Run threw lines of current csv file
-      while($j < count($current_csv))
+      if ( $argv[1] == "noheads" )
       {
-        
-
-        //$content .= $current_csv[$j][$col_name];
-        //$content .= "\n";
-
-
-        // Einfache Ausgabe
-        //echo "Value ".$col_name.":".$current_csv[$j][$col_name]."\n";
-        $sum = $sum + $current_csv[$j][$col_name];
-        
-        $j++;
+          $current_csv = csv_to_array_noheads($src_dir.$filelist[$i]);
+          //$content .= "File: ;".$filelist[$i].";";
+          # Run threw lines of current csv file
+          // var_dump($current_csv);
+          while($j < count($current_csv))
+          {
+          //echo $current_csv[$j][$i];
+          //  var_dump($current_csv[$j]);
+          // var_dump( $current_csv[$j][$spalte]);
+            $sum = $sum + $current_csv[$j][$spalte];
+            $j++;
+          }
+            //$content .= "Average: ;". str_replace(".",",",$sum/count($current_csv)).";";
+            $content .= str_replace(".",",",$sum/count($current_csv)).";";
+            $content .= "\n";
       }
-
-      // Output average
-     // echo "Average ".$col_name.":".$average = $sum / count($current_csv);
-     // echo "\n";
-     
-     $content .= "Average: ;". str_replace(".",",",$sum/count($current_csv)).";";
-     $content .= "\n";
-     // file_put_contents($output_file, $output);
-
+      else      
+      {
+          $current_csv = csv_to_array_heads("src/".$filelist[$i]);
+          $content .= "File: ;".$filelist[$i].";";
+          # Run threw lines of current csv file
+          while($j < count($current_csv))
+          {
+            $sum = $sum + $current_csv[$j][$col_name];
+            $j++;
+          }
+            $content .= "Average: ;". str_replace(".",",",$sum/count($current_csv)).";";
+            $content .= "\n";
+           
+      }
+    
      fwrite($fp,$content);
+     $i++;
 
-
-
-      # echo $filelist[$i]."\n";
-      //echo "\n";
-      $i++;
     }
-  
-
-
-
-
-/**
- * Example
- */
-
-#$result =  csv_to_array($testfile);
-
-
-#var_dump($result);
-
-#echo $result[4]["Seconds"];
-
-
-
-
-
-
-
-
-
-#var_dump($filelist);  
-#var_dump($csv);
-
-#var_dump($csv[2][0])
-#echo "\n";
-
-
-#echo "Test:";
-#echo $csv[1][1]
 
 
 
@@ -141,21 +116,44 @@ if (is_dir($src_dir)){
  * @copyright Copyright (c) 2010, Jay Williams
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
  */
-function csv_to_array($filename='', $delimiter=';')
+function csv_to_array_heads($filename='', $delimiter=';')
 {
-	if(!file_exists($filename) || !is_readable($filename))
-		return FALSE;
+    if(!file_exists($filename) || !is_readable($filename))
+    return FALSE;
+
+    $header = NULL;
+    $data = array();
+    if (($handle = fopen($filename, 'r')) !== FALSE)
+    {
+      while (($row = fgetcsv($handle, 10000, $delimiter)) !== FALSE)
+      {
+        if(!$header)
+          $header = $row;
+        else
+          $data[] = array_combine($header, $row);
+      }
+      fclose($handle);
+    }
+    return $data;
+}
+
+
+function csv_to_array_noheads($filename='', $delimiter=';')
+{
+  $data = array();
+  //echo "Hello";
+  //echo file_exists($filename);
+  if(!file_exists($filename) || !is_readable($filename))
+    return FALSE;
+  
+
 	
-	$header = NULL;
-	$data = array();
 	if (($handle = fopen($filename, 'r')) !== FALSE)
 	{
-		while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
-		{
-			if(!$header)
-				$header = $row;
-			else
-				$data[] = array_combine($header, $row);
+		while (($row = fgetcsv($handle, 10000, $delimiter)) !== FALSE)
+		{	
+        // var_dump($row);
+        $data[] = $row;
 		}
 		fclose($handle);
 	}
